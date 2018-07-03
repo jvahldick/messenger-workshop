51735f046a05b9c756ee81d7c0bd17512d25cf9c
Samuel ROZE <samuel.roze@gmail.com>
2018-07-03 08:43:17 +0100

    [never do this] Faking the authentication for showcase purposes

--- src/CommandHandler/RegisterBetHandler.php
+++ src/CommandHandler/RegisterBetHandler.php
@@ -5,19 +5,23 @@ namespace App\CommandHandler;
 use App\Command\RegisterBet;
 use App\Entity\Bet;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class RegisterBetHandler
 {
     private $registry;
+    private $tokenStorage;
 
-    public function __construct(RegistryInterface $registry)
+    public function __construct(RegistryInterface $registry, TokenStorageInterface $tokenStorage)
     {
         $this->registry = $registry;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function __invoke(RegisterBet $command)
     {
         $bet = new Bet();
+        $bet->username = $this->tokenStorage->getToken()->getUsername();
         $bet->game = $command->getGame();
         $bet->rightScore = $command->getRightScore();
         $bet->leftScore = $command->getLeftScore();
