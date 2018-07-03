245d33f6069141ef202163c5643a042cbd703167
Samuel ROZE <samuel.roze@gmail.com>
2018-07-03 11:38:39 +0100

    Identify that the given message has been received

--- src/Middleware/AuditMiddleware.php
+++ src/Middleware/AuditMiddleware.php
@@ -2,17 +2,28 @@
 
 namespace App\Middleware;
 
+use Symfony\Component\Messenger\Asynchronous\Transport\ReceivedMessage;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\EnvelopeAwareInterface;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 
-class AuditMiddleware implements MiddlewareInterface
+class AuditMiddleware implements MiddlewareInterface, EnvelopeAwareInterface
 {
     /**
+     * @param Envelope $envelope
+     *
      * {@inheritdoc}
      */
-    public function handle($message, callable $next)
+    public function handle($envelope, callable $next)
     {
+        $message = $envelope->getMessage();
+
         try {
-            echo sprintf('Started with message "%s"'."\n", get_class($message));
+            if (null !== $envelope->get(ReceivedMessage::class)) {
+                echo sprintf('Received message "%s"' . "\n", get_class($message));
+            } else {
+                echo sprintf('Started with message "%s"' . "\n", get_class($message));
+            }
 
             return $next($message);
         } finally {
